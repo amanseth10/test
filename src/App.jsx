8eb5ef13ead3e7afa39c867600fe2881bcf60757
amanseth10 <amanseth2626@gmail.com>
2024-05-19 23:35:00 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import axios from "axios"
+import { useState } from "react";
+
 
 function App() {
+  const [head,sethead]=useState([])
+  axios.get("https://internproject-4m4h.onrender.com/get-head/")
+        .then((res) => {
+          // console.log(res.data);
+            sethead(res.data);
+        })
+        .catch((error) => { console.log(error);})
   return (
     <>
-      <h1>Hello</h1>
+      <h1>{JSON.stringify(head)}</h1>
     </>
   )
 }
